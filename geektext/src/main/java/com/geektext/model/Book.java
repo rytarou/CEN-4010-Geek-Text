4c3449035accdca56727cb09ff7184e889a4995c
andrewschwarte <andrewschwarte@gmail.com>
2024-11-11 20:29:30 -0500

    update to Book Class

--- geektext/src/main/java/com/geektext/model/Book.java
+++ geektext/src/main/java/com/geektext/model/Book.java
@@ -1,5 +1,110 @@
 package com.geektext.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "books")
 public class Book {
 
+    @Id
+    private String id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String genre;
+    private String publisher;
+    private Double price;
+    private Integer yearPublished;
+    private Integer copiesSold;
+    private Double rating;
+    private String description;
+
+    // Getters and Setters
+    public String getId() {
+        return id; 
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(Integer yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public Integer getCopiesSold() {
+        return copiesSold;
+    }
+
+    public void setCopiesSold(Integer copiesSold) {
+        this.copiesSold = copiesSold;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
