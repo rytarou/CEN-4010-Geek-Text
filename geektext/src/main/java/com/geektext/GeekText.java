911231301cd89bf953eafac659c65b57e6dd992f
Dullo33 <david.a.ulloa@gmail.com>
2024-11-29 22:22:25 -0500

    Add files via upload

---
+++ geektext/src/main/java/com/geektext/GeekText.java
@@ -0,0 +1,12 @@
+package com.geektext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekText {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekText.class, args);
+    }
+}
