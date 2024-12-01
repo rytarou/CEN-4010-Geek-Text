23c276d4f93f3d886050fa280465a786bcb55311
mic7aelr <84257374+mic7aelr@users.noreply.github.com>
2024-11-30 21:59:46 -0500

    Create User.java

---
+++ geektext/src/main/java/com/geektext/model/User.java
@@ -0,0 +1,57 @@
+package com.geektext.usercreation;
+
+import org.springframework.data.annotation.Id;
+import java.util.List;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users") 
+public class User {
+    
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private List<CreditCard> creditCards; 
+    
+    public List<CreditCard> getCreditCards() {
+        return creditCards;
+    }
+
+    public void setCreditCards(List<CreditCard> creditCards) {
+        this.creditCards = creditCards;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
